6cf5251e393a9a38a19147b252555822bb16b46c
lfgSammy <sammylovesdesign@gmail.com>
2026-06-15 13:28:48 +0100

    Add pagination settings to REST framework configuration

--- blogplatform/settings.py
+++ blogplatform/settings.py
@@ -63,6 +63,9 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_SCHEMA_CLASS':'drf_spectacular.openapi.AutoSchema',
+
+    'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZES':10,
 }
 
 cloudinary.config(
