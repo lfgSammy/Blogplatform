615d56cf2ac503afa16d7d305db317959a090c8e
lfgSammy <sammylovesdesign@gmail.com>
2026-06-19 11:52:16 +0100

    Remove unused import of send_mail from signals.py

--- posts/signals.py
+++ posts/signals.py
@@ -1,6 +1,5 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.core.mail import send_mail
 from django.conf import settings
 from .models import Post, Comment, Like, Profile
 from django.contrib.auth.models import User
